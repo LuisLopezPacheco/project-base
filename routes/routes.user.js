bb48ebf8c7e94c289303e157cfeee9b132a6c946
LuisLopezPacheco <luis.lopez0413@alumnos.udg.mx>
2023-03-27 17:02:38 -0600

    error handling implementation for route

--- routes/routes.user.js
+++ routes/routes.user.js
@@ -5,11 +5,26 @@ const services = require("../services/user.services");
 const user = new services();
 
 router.get('/', async (req, res, next) => {
-  const respusta = await user.getUsers();
-  res.json(respusta);
+  try {
+    const respusta = await user.getUsers();
+    res.json(respusta);
+  }catch (err) {
   // res.json({ info: 'Node.js, Express, and Postgres API' })
-  next();
+     next(err);
+    // throw new Error('Error al obtener usuarios');
+  }
 })
+
+// router.get('/', async (req, res, next) => {
+//   return new Promise((resolve, reject) => {
+//     try{
+//     const respusta = user.getUsers();
+//     resolve.json(respusta);
+//     }catch (reject) {
+//       next(Error);
+//     }
+//   });
+// })
 
 module.exports = router
 
